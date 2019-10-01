073c379592086481112ee0a3734b415f977562fc
Stephen Daniel <stephen.daniel@students.plymouth.ac.uk>
2019-10-01 20:28:45 +0100

    helloworld

---
+++ helloworld/src/helloworld/helloworld.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 1;i < 11; i++)
+	
+		{
+			System.out.println("helloword #" + i);
+	}
+	}
+}
